36119b556ec98cc586a13399fdc9ab989f8b50bf
urapotapovich <poyapovich22819@gmail.com>
2026-07-27 09:02:49 +0300

    Initial Commit

---
+++ MOEX/MOEXApp.swift
@@ -0,0 +1,17 @@
+//
+//  MOEXApp.swift
+//  MOEX
+//
+//  Created by Юра Потапович on 27.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct MOEXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
