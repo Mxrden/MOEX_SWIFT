aa5b02acb9232932c675cd2793eed2883f4739ec
urapotapovich <poyapovich22819@gmail.com>
2026-07-28 10:59:34 +0300

    feat: Добавлено больше полей локализации

--- MOEX/MOEXApp.swift
+++ MOEX/MOEXApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct MOEXApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SharesView()
         }
     }
 }
--- MOEX/MainTabView.swift
+++ MOEX/MainTabView.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct MainTabView: View {
+    @State private var selectedTab = 0
+    @EnvironmentObject var settings: AppSettings
+    
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            SharesView()
+                .tabItem{
+                    Text("Shares")
+                }
+                .tag(0)
+            IndexesView()
+                .tabItem{
+                    Text("Indexes")
+                }
+                .tag(1)
+            SettingsVIew()
+                .tabItem{
+                    Text("Settings")
+                }
+                .tag(2)
+        }
+    }
+}
+
