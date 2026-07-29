a1d3802d5c10eadca49290bbfffa66979dfe8dc0
urapotapovich <poyapovich22819@gmail.com>
2026-07-29 07:32:41 +0300

    fix: Исправлена модель данных для индекса

--- MOEX/Model/Index.swift
+++ MOEX/Model/Index.swift
@@ -1,5 +1,5 @@
 //
-//  Share.swift
+//  Index.swift
 //  MOEX
 //
 //  Created by Юра Потапович on 28.07.2026.
@@ -10,38 +10,33 @@ import Foundation
 struct Index {
     let secid: String
     let shortName: String?
-    let prevPrice: Double?
-    let lastChangePrcnt: Double?
+    let last: Double?
+    let changePrcnt: Double?
     let open: Double?
     let low: Double?
     let high: Double?
-    let volToday: Int?
-    let sysTime: Date?
-    
+    let value: Double?
+    let time: Date?
+
     init?(dictionary: [String: Any]) {
         guard let secid = dictionary["SECID"] as? String else { return nil }
         self.secid = secid
         self.shortName = dictionary["SHORTNAME"] as? String
-        self.prevPrice = dictionary["PREVPRICE"] as? Double
-        self.lastChangePrcnt = dictionary["LASTCHANGEPRCNT"] as? Double
+        self.last = dictionary["LAST"] as? Double
+        self.changePrcnt = dictionary["CHANGEPRCNT"] as? Double
         self.open = dictionary["OPEN"] as? Double
         self.low = dictionary["LOW"] as? Double
         self.high = dictionary["HIGH"] as? Double
-        
-        if let vol = dictionary["VOLTODAY"] as? Int {
-            self.volToday = vol
-        } else if let volStr = dictionary["VOLTODAY"] as? String, let vol = Int(volStr) {
-            self.volToday = vol
-        } else {
-            self.volToday = nil
-        }
-        
-        //Парсинг
-        if let sysStr = dictionary["SYSTIME"] as? String {
-            self.sysTime = DateFormatter.moexDateTime.date(from: sysStr)
-        } else {
-            self.sysTime = nil
-        }
+        self.value = dictionary["VALUE"] as? Double
 
+        //Парсинг
+        if let timeStr = dictionary["TIME"] as? String {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "HH:mm:ss"
+            formatter.locale = Locale(identifier: "en_US_POSIX")
+            self.time = formatter.date(from: timeStr)
+        } else {
+            self.time = nil
+        }
     }
 }
