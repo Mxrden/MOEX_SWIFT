f1d852bcd32d0f8f272d3bf02df161b00b802e7a
urapotapovich <poyapovich22819@gmail.com>
2026-07-30 11:31:10 +0300

    feat: Добавлена строка для будущей таблицы ShareRow

--- MOEX/View/ShareRow.swift
+++ MOEX/View/ShareRow.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct ShareRow: View {
+    let share: Share
+    
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text(share.shortName ?? share.secid).font(.headline)
+                Text(share.secid).font(.caption).foregroundStyle(.secondary)
+            }
+            
+            Spacer()
+            
+            VStack (alignment: .trailing){
+                if let price = share.prevPrice {
+                    Text(String(format: "%.2f", price)).font(.headline)
+                }
+                if let change = share.lastChangePrcnt {
+                    Text(String(format: "%.2f", change)).foregroundColor(change >= 0 ? .green : .red)
+                }
+            }
+        }
+        .padding(.vertical, 4)
+    }
+}
